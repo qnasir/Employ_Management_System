70bb031ce5c220204710d1b528fcb184efb09d66
qnasir <qnasir575@gmail.com>
2024-11-20 10:58:57 +0530

    Inheritance

--- index.cpp
+++ index.cpp
@@ -18,3 +18,57 @@ public:
 
     virtual void calculateSalary() = 0; // Pure virtual function
 };
+
+class Manager : public Employee {
+    int teamSize;
+    double bonus;
+
+public:
+    Manager(string n, int i, string dept, int t, double b)
+        : Employee(n, i, dept), teamSize(t), bonus(b) {}
+
+    void displayDetails() override {
+        Employee::displayDetails();
+        cout << "Team Size: " << teamSize << ", Bonus: $" << bonus << endl;
+    }
+
+    void calculateSalary() override {
+        cout << "Manager Salary: $" << (5000 + bonus) << endl;
+    }
+};
+
+class Engineer : public Employee {
+    string specialization;
+    int projectCount;
+
+public:
+    Engineer(string n, int i, string dept, string spec, int p)
+        : Employee(n, i, dept), specialization(spec), projectCount(p) {}
+
+    void displayDetails() override {
+        Employee::displayDetails();
+        cout << "Specialization: " << specialization
+             << ", Projects: " << projectCount << endl;
+    }
+
+    void calculateSalary() override {
+        cout << "Engineer Salary: $" << (4000 + projectCount * 200) << endl;
+    }
+};
+
+class Intern : public Engineer {
+    double stipend;
+
+public:
+    Intern(string n, int i, string dept, string spec, int p, double s)
+        : Engineer(n, i, dept, spec, p), stipend(s) {}
+
+    void displayDetails() override {
+        Engineer::displayDetails();
+        cout << "Stipend: $" << stipend << endl;
+    }
+
+    void calculateSalary() override {
+        cout << "Intern Salary: $" << stipend << endl;
+    }
+};
